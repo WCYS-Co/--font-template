googlefonts/googlefonts-project-template

Change `make update-ufr` to `make update-project-template` and update old URL + README (#79)
* Update "make update-ufr" for the post-ufr repo

This changes "make update-ufr" to "make update-project-template"
and updates the URL that command points to. It also updates the
main README.

* Change UFR to Project Template in the README

Makefile
@@ -45,8 +45,8 @@ clean:
 	rm -rf venv
 	find . -name "*.pyc" | xargs rm delete
 
-update-ufr:
-	npx update-template https://github.com/googlefonts/Unified-Font-Repository/
+update-project-template:
+	npx update-template https://github.com/googlefonts/googlefonts-project-template/
 
 update:
 	pip install --upgrade $(dependency); pip freeze > requirements.txt

WCYS-Co/--font-template

Makefile
@@ -45,7 +45,7 @@ clean:
 	rm -rf venv
 	find . -name "*.pyc" | xargs rm delete
 
-update-ufr:
+update-font-template:
 	npx update-template https://github.com/WCYS-Co/--font-template/
 
 update: